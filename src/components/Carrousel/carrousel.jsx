e7f65ee527d3eefdfd95fc3ec9e422c1c0c93426
Santiago Cuadros Mateus <79917786+sacuadros09@users.noreply.github.com>
2025-04-16 17:00:07 -0500

    carrousel

--- src/components/Carrousel/carrousel.jsx
+++ src/components/Carrousel/carrousel.jsx
@@ -2,15 +2,31 @@ import "./carrousel.css";
 
 export function Carrousel({ items }) {
   return (
-    <div className="carrousel-container">
-      <div className="carrousel">
-        {items.map((item, index) => (
-          <button key={index} className="carrousel-button">
-            <img src={item.icon} alt={item.text} className={`carrousel-icon ${item.className}`} />
-            <span className="carrousel-text">{item.text}</span>
-          </button>
-        ))}
+    <div className="carrousel-section">
+      {/* Línea superior */}
+      <div className="carrousel-line"></div>
+
+      {/* Contenedor del carrusel */}
+      <div className="carrousel-container">
+        <div className="carrousel">
+          {items.map((item, index) => (
+            <button key={`original-${index}`} className="carrousel-button">
+              <img src={item.icon} alt={item.text} className={`carrousel-icon ${item.className}`} />
+              <span className="carrousel-text">{item.text}</span>
+            </button>
+          ))}
+          {/* Duplicar los elementos para el bucle infinito */}
+          {items.map((item, index) => (
+            <button key={`duplicate-${index}`} className="carrousel-button">
+              <img src={item.icon} alt={item.text} className={`carrousel-icon ${item.className}`} />
+              <span className="carrousel-text">{item.text}</span>
+            </button>
+          ))}
+        </div>
       </div>
+
+      {/* Línea inferior */}
+      <div className="carrousel-line"></div>
     </div>
   );
 }
